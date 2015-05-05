ebb5ad562a4c546e6245cd11c8aa6930905facad
Will Iverson <wiverson@gmail.com>
2015-05-05 16:20:33 -0700

    Hello World checkin

---
+++ src/main/java/com/dev9/crash/CrashDummyApplication.java
@@ -0,0 +1,12 @@
+package com.dev9.crash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrashDummyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrashDummyApplication.class, args);
+    }
+}
