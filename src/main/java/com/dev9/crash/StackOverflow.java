d994c1a22171686e51d959a22e851a30a33ad02b
Will Iverson <wiverson@gmail.com>
2015-05-05 16:36:02 -0700

    StackOverflow

---
+++ src/main/java/com/dev9/crash/StackOverflow.java
@@ -0,0 +1,17 @@
+package com.dev9.crash;
+
+public class StackOverflow implements Runnable {
+
+    private int counter = 0;
+
+    public int addThis(int x) {
+        counter = counter + x;
+        addThis(1);
+        return counter;
+    }
+
+    @Override
+    public void run() {
+        addThis(1);
+    }
+}
--- src/test/java/com/dev9/crash/CrashDummyApplicationTests.java
+++ src/test/java/com/dev9/crash/CrashDummyApplicationTests.java
@@ -2,17 +2,31 @@ package com.dev9.crash;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CrashDummyApplication.class)
 @WebAppConfiguration
 public class CrashDummyApplicationTests {
 
+    @Autowired
+    ApplicationContext context;
+
 	@Test
 	public void contextLoads() {
+        assertThat(context).isNotNull();
 	}
+
+    @Test(expected = StackOverflowError.class)
+    public void stackOverflow() {
+        StackOverflow sf = new StackOverflow();
+        sf.run();
+    }
 
 }
