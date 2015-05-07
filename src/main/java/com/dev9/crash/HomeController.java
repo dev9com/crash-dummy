404db52fec795940607e736f4566d153d3db34f5
Will Iverson <wiverson@gmail.com>
2015-05-06 17:06:05 -0700

    Starting to build ui

--- src/main/java/com/dev9/crash/HomeController.java
+++ src/main/java/com/dev9/crash/HomeController.java
@@ -1,13 +1,44 @@
 package com.dev9.crash;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 class HomeController {
 
+    @Autowired
+    ApplicationContext context;
+
     @RequestMapping("/")
     String index() {
         return "index";
+    }
+
+    @ModelAttribute("allBadThings")
+    List<BadThing> allBadThings() {
+        List result = new ArrayList<>(allBadThingsMap().values());
+
+        return result;
+    }
+
+    Map<String, BadThing> allBadThingsMap() {
+        return context.getBeansOfType(BadThing.class);
+    }
+
+    @RequestMapping(value = "/crash", params = {"id"})
+    String crash(String id) throws Exception {
+        BadThing crash = allBadThingsMap().get(id);
+        if (crash != null) {
+            return crash.doBadThing();
+        } else {
+            return null;
+        }
     }
 }
