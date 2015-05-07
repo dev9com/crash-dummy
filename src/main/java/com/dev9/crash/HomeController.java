df70105177f60cf7a5cb2effe63c28c337be2814
Will Iverson <wiverson@gmail.com>
2015-05-07 15:44:54 -0700

    UI now works.

--- src/main/java/com/dev9/crash/HomeController.java
+++ src/main/java/com/dev9/crash/HomeController.java
@@ -1,10 +1,14 @@
 package com.dev9.crash;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +16,8 @@ import java.util.Map;
 
 @Controller
 class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     ApplicationContext context;
@@ -32,13 +38,36 @@ class HomeController {
         return context.getBeansOfType(BadThing.class);
     }
 
-    @RequestMapping(value = "/crash", params = {"id"})
-    String crash(String id) throws Exception {
-        BadThing crash = allBadThingsMap().get(id);
-        if (crash != null) {
-            return crash.doBadThing();
-        } else {
-            return null;
+    BadThing byId(String id) {
+        for (BadThing badThing : allBadThings()) {
+            if (badThing.getBadThingId().equalsIgnoreCase(id))
+                return badThing;
         }
+        return null;
+    }
+
+    private String result;
+
+    @RequestMapping(value = "/crash", method = RequestMethod.POST)
+    String crash(String id) throws Exception {
+        BadThing crash = byId(id);
+        if (crash != null) {
+            log.info("Started {}", id);
+            result = crash.doBadThing();
+            log.info("Finished {}", id);
+        } else {
+            log.info("Can't find {}", id);
+        }
+
+        return "crash";
+    }
+
+    @ModelAttribute("result")
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
     }
 }
