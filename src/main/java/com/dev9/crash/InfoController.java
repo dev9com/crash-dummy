5401395bb754a21fbbe4a14a2f45702a389757f7
Will Iverson <wiverson@gmail.com>
2015-07-23 16:14:22 -0700

    Add UI for finding class files on disk

--- src/main/java/com/dev9/crash/InfoController.java
+++ src/main/java/com/dev9/crash/InfoController.java
@@ -1,11 +1,13 @@
 package com.dev9.crash;
 
+import com.dev9.crash.info.ClassFileLocator;
 import com.dev9.crash.info.WebServerInformation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +29,22 @@ public class InfoController {
         model.addAttribute("info", info.allProperties(request, servletContext));
 
         return "server";
+    }
+
+
+    @RequestMapping("/class")
+    String server(@RequestParam(value = "clazz", required = false) String clazz, Model model) {
+
+        ClassFileLocator classFileLocator = new ClassFileLocator();
+
+        String search = "java.lang.String";
+        if (clazz != null)
+            search = clazz;
+
+        model.addAttribute("clazz", search);
+        model.addAttribute("clazzLocation", classFileLocator.find(search));
+
+        return "clazz";
     }
 
 }
